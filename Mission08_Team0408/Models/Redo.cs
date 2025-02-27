bcb53fee480b9ee999d67e8125137aae5ef63bcc
fitzkai <fitzkaitlyn12@gmail.com>
2025-02-26 18:31:29 -0700

    oops I forgot to make a new branch

--- Mission08_Team0408/Models/Redo.cs
+++ Mission08_Team0408/Models/Redo.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mission08_Team0408.Models
 {
@@ -11,7 +12,9 @@ namespace Mission08_Team0408.Models
         public string? DueDate { get; set; }
         [Required]
         public int Quadrant { get; set; }
-        public string? Category { get; set; }
+        [ForeignKey("CategoryId")]
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
         public bool? Completed { get; set; }
     }
 }
