701078aa46c56b9c6ddc7694d8af9cdbb6276f71
fitzkai <fitzkaitlyn12@gmail.com>
2025-02-26 19:00:28 -0700

    Renaming categories table

---
+++ Mission08_Team0408/Migrations/20250227015749_Initial2.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission08_Team0408.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_Category_CategoryId",
+                table: "ToDos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Categories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_Categories_CategoryId",
+                table: "ToDos",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_Categories_CategoryId",
+                table: "ToDos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Category");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_Category_CategoryId",
+                table: "ToDos",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "CategoryId");
+        }
+    }
+}
